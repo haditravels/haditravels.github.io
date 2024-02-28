fcccfb5c2fc77637d944f98ad71cebbf627cf0f7
Hamza Durrani <Info@Thecodingimpact.Com>
2024-02-28 23:56:09 +0500

    script update

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ let navbar = document.querySelector(".header .navbar");
 document.querySelector("#menu-btn").onclick = () => {
   navbar.classList.toggle("active");
 };
-<<<<<<< HEAD
 
 // video slider
 document
@@ -14,7 +13,6 @@ document
       document.querySelector(".about .video-container .video").src = src;
     };
   });
-=======
 //form backend start
 // Initialize Firebase
 var firebaseConfig = {
@@ -25,7 +23,7 @@ var firebaseConfig = {
   storageBucket: "haditravels-ef4b0.appspot.com",
   messagingSenderId: "212772020766",
   appId: "1:212772020766:web:79b0ab9de3b7ae9ad8440f",
-  measurementId: "G-9ECQV4PQSX"
+  measurementId: "G-9ECQV4PQSX",
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
@@ -34,53 +32,51 @@ firebase.initializeApp(firebaseConfig);
 const database = firebase.database();
 
 // Get the form element
-const form = document.querySelector('#book-form');
+const form = document.querySelector("#book-form");
 
 // ... (previous code)
 
 // Add submit event listener
-form.addEventListener('submit', async (e) => {
+form.addEventListener("submit", async (e) => {
   e.preventDefault();
 
   // Get input field values
-  const place = document.querySelector('#place').value;
-  const date = document.querySelector('#date').value;
-  const contact = document.querySelector('#contact').value;
-  const peopleNo = document.querySelector('#peopleno').value;
+  const place = document.querySelector("#place").value;
+  const date = document.querySelector("#date").value;
+  const contact = document.querySelector("#contact").value;
+  const peopleNo = document.querySelector("#peopleno").value;
 
   // Save data to Firebase
-  database.ref('orders').push({
+  database.ref("orders").push({
     place,
     date,
     contact,
-    peopleNo
+    peopleNo,
   });
-  alert('Your request is recorded. Our team will contact you soon');
+  alert("Your request is recorded. Our team will contact you soon");
   // Initialize Email.js with the Public Key
   emailjs.init("VTKZBdwbBaxvZytYk");
 
   // Prepare email template parameters
   const templateParams = {
-    to_email: 'teapack534@gmail.com',
-    from_name: 'The Coding Impact',
-    subject: 'New Order',
-    message: `Place: ${place}\nDate: ${date}\nContact Number: ${contact}\nPeople no: ${peopleNo}`
+    to_email: "teapack534@gmail.com",
+    from_name: "The Coding Impact",
+    subject: "New Order",
+    message: `Place: ${place}\nDate: ${date}\nContact Number: ${contact}\nPeople no: ${peopleNo}`,
   };
 
   // Define your Email.js service ID and template ID
-  const emailjsServiceId = 'service_ru36ova';
-  const emailjsTemplateId = 'template_mug39bn';
+  const emailjsServiceId = "service_ru36ova";
+  const emailjsTemplateId = "template_mug39bn";
 
   // Send email using Email.js
-  emailjs.send(emailjsServiceId, emailjsTemplateId, templateParams)
+  emailjs
+    .send(emailjsServiceId, emailjsTemplateId, templateParams)
     .then((response) => {
-      console.log('Email sent successfully:', response);
-        location.reload();
-
+      console.log("Email sent successfully:", response);
+      location.reload();
     })
     .catch((error) => {
-      console.error('Error sending email:', error);
-  
+      console.error("Error sending email:", error);
     });
 });
->>>>>>> 4b291677f4525d9a638fe920fa78f6140b809980
