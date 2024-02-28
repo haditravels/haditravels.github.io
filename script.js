9013a80ee7a86c622d125f0716a9a102a24ce77e
Hamza Durrani <Info@Thecodingimpact.Com>
2024-02-28 23:50:05 +0500

    day 2 progress

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ let navbar = document.querySelector(".header .navbar");
 document.querySelector("#menu-btn").onclick = () => {
   navbar.classList.toggle("active");
 };
+
+// video slider
+document
+  .querySelectorAll(".about .video-container .controls .control-btn")
+  .forEach((btn) => {
+    btn.onclick = () => {
+      let src = btn.getAttribute("data-src");
+      document.querySelector(".about .video-container .video").src = src;
+    };
+  });
